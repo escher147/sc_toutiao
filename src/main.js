b8bcc434b56556d913b4bb2fecb8d494628df863
escher <celestialescher@gmail.com>
2021-07-10 09:15:30 +0800

    调整目录结构

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 导入全局样式
+import './assets/css/global.less'
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
